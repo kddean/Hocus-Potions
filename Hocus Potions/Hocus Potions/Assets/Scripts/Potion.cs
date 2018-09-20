54ca59e88a12e782a0841213b92684efa5966aab
Sam Mittelhaeuser <sammittelhaeuser@gmail.com>
2018-09-20 15:32:40 -0600

    Fixing brewing

---
+++ Hocus Potions/Hocus Potions/Assets/Scripts/Potion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : MonoBehaviour {
+    //There's probably a better way to do this but it works for now
+   // public enum Attributes { sleep, healing, transformation, posion, invisiblity, magicPP, magicMM, cat, sheep, chicken };
+
+    string name;
+    Sprite image;
+
+    int duration;  //in whole hours
+    Ingredient.Attributes primary;
+    Ingredient.Attributes? secondary;
+    Ingredient.Modifiers? modifier;
+
+    public Potion(string name, Sprite image, int duration, Ingredient.Attributes primary, Ingredient.Attributes? secondary, Ingredient.Modifiers? modifier) {
+        this.name = name;
+        this.image = image;
+        this.duration = duration;
+        this.primary = primary;
+        this.secondary = secondary;
+        this.modifier = modifier;
+    }
+}
