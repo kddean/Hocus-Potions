58288e55b3a1bbdbc5e6d05e1879e5d38055b710
Khadeidre Dean <khadeidredean@gmail.com>
2018-11-06 15:20:32 -0700

    Spawning for Gathering is working. Still need to fix a few bugs with it.

--- Hocus Potions/Assets/Scripts/Gathering.cs
+++ Hocus Potions/Assets/Scripts/Gathering.cs
@@ -7,56 +7,43 @@ public class Gathering : MonoBehaviour {
 
     public GameObject spawnedItem;
     ResourceLoader rl;
-    public SpriteRenderer[] sprites;
-    public SpriteRenderer sprite;
+    SpriteRenderer[] sprites;
+    public Sprite sprite;
     public int ran;
     public List<string> plants;
 
     // Use this for initialization
     void Start () {
-        /*rl = GameObject.FindGameObjectWithTag("loader").GetComponent<ResourceLoader>();
- 
-        plants = rl.ingredients.Keys.ToList();
-        int i = 0;
-        sprites = new SpriteRenderer[rl.ingredients.Count];
-        foreach (string item in plants)
+        rl = GameObject.FindGameObjectWithTag("loader").GetComponent<ResourceLoader>();
+        //Display plant if spawener has one else go get one
+        GatheringManager.SpawnerData temp;
+        if (rl.gatheringManager.spawnerData.TryGetValue(gameObject.name, out temp))
         {
-            Debug.Log(item);
-            Debug.Log(i);
-            Debug.Log(rl.ingredients[item].image);
-            this.sprites[i].sprite = Resources.Load<Sprite>("Plants/" + rl.ingredients[item].image);
-            
-            if (i < rl.ingredients.Count)
-            {
-                i++;
-            }
+            this.GetComponent<SpriteRenderer>().sprite = GetComponent<SpriteRenderer>().sprite = rl.ingredients[temp.spawnedItem].image;
+        }
+        else
+        {
+            rl.gatheringManager.Populate(this);
         }
 
-        ran = Random.Range(0, rl.ingredients.Count);
-        SpriteRenderer sprite = new SpriteRenderer();
+        /*rl = GameObject.FindGameObjectWithTag("loader").GetComponent<ResourceLoader>();
         sprite.sprite = Resources.Load<Sprite>("Plants/poppy_inv");
-
-        //Instantiate(sprites[0].sprite, this.transform.position, Quaternion.identity);
         Instantiate(sprite.sprite, this.transform.position, Quaternion.identity);*/
-
-
-
-        rl = GameObject.FindGameObjectWithTag("loader").GetComponent<ResourceLoader>();
-        sprite.sprite = Resources.Load<Sprite>("Plants/poppy_inv");
-        Instantiate(sprite.sprite, this.transform.position, Quaternion.identity);
 
     }
 
     // Update is called once per frame
     void Update () {
-		
+
 	}
 
     private void OnMouseDown()
     {
-        rl.inv.Add(rl.ingredients["poppy"], rl.ingredients["poppy"].name, rl.ingredients["poppy"].image);
-        for (int i = 0; i < rl.ingredients.Count; i++) {
-            sprite.sprite = null;
-        }
+        GatheringManager.SpawnerData temp;
+        rl.gatheringManager.spawnerData.TryGetValue(gameObject.name, out temp);
+        rl.inv.Add(rl.ingredients[temp.spawnedItem], rl.ingredients[temp.spawnedItem].name, rl.ingredients[temp.spawnedItem].image);
+
+        this.GetComponent<SpriteRenderer>().sprite = null;
+        rl.gatheringManager.spawnerData.Remove(gameObject.name);
     }
 }
