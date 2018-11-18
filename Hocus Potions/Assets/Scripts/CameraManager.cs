8d36f42c7016aba2e817aa98e34c5fc3360227e0
Sam Mittelhaeuser <sammittelhaeuser@gmail.com>
2018-11-17 17:27:48 -0700

    Camera tracking
    Camera tracks player in overworld but won't go off the side of the map. Player can still walk off but the camera won't follow them.

---
+++ Hocus Potions/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+    public float[] xBounds, yBounds;
+    Player player;
+    Vector3 pos;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pos = player.transform.position;
+        pos.x = Mathf.Clamp(pos.x, xBounds[0], xBounds[1]);
+        pos.y = Mathf.Clamp(pos.y, yBounds[0], yBounds[1]);
+        pos.z = -10;
+        transform.position = pos;
+	}
+}
