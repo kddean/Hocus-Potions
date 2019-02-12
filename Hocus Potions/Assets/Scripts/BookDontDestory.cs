331e9983d2b9e5d114a067b944d900d559b83293
Khadeidre Dean <khadeidredean@gmail.com>
2019-02-12 13:17:25 -0700

    Added Basic Book UI

---
+++ Hocus Potions/Assets/Scripts/BookDontDestory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookDontDestory : MonoBehaviour {
+    public void Awake()
+    {
+        DontDestroyOnLoad(this);
+        if (Resources.FindObjectsOfTypeAll(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Hocus Potions/Assets/Scripts/BookManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookManager : MonoBehaviour {
+
+    public GameObject BookCanvas;
+    public GameObject PlantTab;
+    public GameObject PotionTab;
+    public GameObject MapTab;
+    public string CurrentTab;
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(this);
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        BookCanvas = GameObject.Find("BookCanvas");
+        PlantTab = GameObject.Find("PlantTab");
+        PotionTab = GameObject.Find("PotionTab");
+        MapTab = GameObject.Find("MapTab");
+
+        BookCanvas.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("m"))
+        {
+            if (BookCanvas.activeSelf == false)
+            {
+                BookCanvas.SetActive(true);
+            }
+            else if (BookCanvas.activeSelf == true)
+            {
+                BookCanvas.SetActive(false);
+            }
+        }
+	}
+
+    public void SetCurrentTab(int i)
+    {
+        if (i == 0)
+        {
+            Vector3 temp = PlantTab.transform.localPosition;
+            temp.x *= -1;
+            PlantTab.transform.localPosition = temp;
+            PlantTab.transform.localScale *= -1;
+            CurrentTab = "PlantTab";
+        }
+        else if (i == 1)
+        {
+            Vector3 temp = PotionTab.transform.localPosition;
+            temp.x *= -1;
+            PotionTab.transform.localPosition = temp;
+            PotionTab.transform.localScale *= -1;
+            CurrentTab = "PotionTab";
+        }
+        else if (i == 2)
+        {
+            Vector3 temp = MapTab.transform.localPosition;
+            temp.x *= -1;
+            MapTab.transform.localPosition = temp;
+            MapTab.transform.localScale *= -1;
+            CurrentTab = "MapTab";
+        }
+    }
+}
